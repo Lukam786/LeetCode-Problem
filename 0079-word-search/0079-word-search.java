0959b2c4c207d6a21e3e4abd72f3770ea0cb56f0
Mohammad Lukam Mansuri <lookman21102001@gmail.com>
2026-09-11 22:36:54 +0530

    Time: 130 ms (71.3%), Space: 43.1 MB (43.48%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (backtrack(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean backtrack(char[][] board, String word,
+                              int row, int col, int index) {
+
+        // Word completely found
+        if (index == word.length()) {
+            return true;
+        }
+
+        // Out of bounds
+        if (row < 0 || row >= board.length ||
+            col < 0 || col >= board[0].length) {
+            return false;
+        }
+
+        // Character doesn't match
+        if (board[row][col] != word.charAt(index)) {
+            return false;
+        }
+
+        // Mark this cell as visited
+        char temp = board[row][col];
+        board[row][col] = '#';
+
+        // Move in 4 directions
+        boolean found =
+                backtrack(board, word, row - 1, col, index + 1) ||
+                backtrack(board, word, row + 1, col, index + 1) ||
+                backtrack(board, word, row, col - 1, index + 1) ||
+                backtrack(board, word, row, col + 1, index + 1);
+
+        // Backtrack: restore the cell
+        board[row][col] = temp;
+
+        return found;
+    }
+}
