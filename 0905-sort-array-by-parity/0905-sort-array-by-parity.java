acd9db90c2c6b7c618d5fc81c0e9168bd2c89d62
Mohammad Lukam Mansuri <lookman21102001@gmail.com>
2026-09-19 12:30:54 +0530

    Time: 0 ms (100%), Space: 46.7 MB (69.04%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,25 +1,14 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-
-        int[] ans = new int[nums.length];
-        int index = 0;
-
-        // Put even numbers first
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                ans[index] = nums[i];
-                index++;
+        int left=0; 
+        for(int i=0; i<nums.length; i++){
+            if((nums[i]%2) ==0){
+                int temp=nums[i];
+                nums[i]=nums[left];
+                nums[left]=temp;
+                left++;
             }
         }
-
-        // Put odd numbers after that
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 != 0) {
-                ans[index] = nums[i];
-                index++;
-            }
-        }
-
-        return ans;
+        return nums;
     }
 }
