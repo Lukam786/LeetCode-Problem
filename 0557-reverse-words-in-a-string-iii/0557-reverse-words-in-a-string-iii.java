c27a1ec48a6cb4b1977c928e158bb39d1aea50cd
Mohammad Lukam Mansuri <lookman21102001@gmail.com>
2026-09-23 13:24:55 +0530

    Time: 31 ms (10.45%), Space: 47.4 MB (10.41%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reverseWords(String s) {
+        Stack<Character> st=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length(); i++){
+            if(s.charAt(i) != ' '){
+                st.push(s.charAt(i));
+            }
+            else{
+                while(!st.isEmpty()){
+                 sb.append(st.pop());
+                } 
+                 sb.append(' ');  
+            } 
+           
+        }
+          while(!st.isEmpty()){
+                sb.append(st.pop());
+            }
+            return sb.toString();
+        
+    }
+}
